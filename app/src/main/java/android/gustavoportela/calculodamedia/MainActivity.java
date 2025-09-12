feb40f9fc1642f9952d6ab81759f722c4f586906
GUSTAVO PORTELA DE LIMA <GUSTAVO_4657@uirapuru.br>
2025-09-12 15:11:59 -0300

    tela finalizada

--- app/src/main/java/android/gustavoportela/calculodamedia/MainActivity.java
+++ app/src/main/java/android/gustavoportela/calculodamedia/MainActivity.java
@@ -1,5 +1,6 @@
 package android.gustavoportela.calculodamedia;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -23,34 +24,30 @@ public class MainActivity extends AppCompatActivity {
     private String nome;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        initComponentes();
-
-        btnCalcular.setOnClickListener(view->{
-            validaCampos();
-            calculaMedia():
-        });
-
-
-
-
-
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         initComponentes();
 
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnCalcular.setOnClickListener(view -> {
+            validaCampos();
+            calculaMedia();
         });
 
 
-    }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+
+
+        }
 
     private void calculaMedia() {
         double n1 = Double.parseDouble(nota1.getText().toString());
@@ -60,35 +57,36 @@ public class MainActivity extends AppCompatActivity {
         double media = (n1 + n2 + n3 + n4) / 4;
         double faltas = Double.parseDouble(numeroFaltas.getText().toString());
 
-        if (media > 7){
-            if (faltas < 20){
+        if (media > 7) {
+            if (faltas < 20) {
                 resultado.setTextColor(Color.parseColor("#437845"));
-            }else{
+                resultado.setText("aluno aprovado com média" + media);
+            } else {
                 resultado.setTextColor(Color.parseColor("#F44336"));
+                resultado.setText("aluno com excesso de faltas" + faltas);
 
-            }else
+            }
+        } else {
+            resultado.setTextColor(Color.parseColor("#F44336"));
+            resultado.setText("Aluno retido com média " + media);
         }
 
 
     }
 
-    private void validaCampos() {
-        if (TextUtils.isEmpty(nota1.getText())){
-            nota1.setError("este campo nao pode estar vazio...");
-        } else if (TextUtils.isEmpty(nota2.getText())) {
-            nota1.setError("este campo nao pode estar vazio...");
-        } else if (TextUtils.isEmpty(nota3.getText())) {
-            nota1.setError("este campo nao pode estar vazio...");
-        } else if (TextUtils.isEmpty(nota4.getText())) {
-            nota1.setError("este campo nao pode estar vazio...");
-        }
+    private boolean validaCampos() {
+        return nota1.getText().toString().isEmpty()
+                && nota2.getText().toString().isEmpty()
+                && nota3.getText().toString().isEmpty()
+                && nota4.getText().toString().isEmpty();
     }
+
 
     private void initComponentes() {
         nota1 = findViewById(R.id.nota1_et);
-        nota1 = findViewById(R.id.nota2_et);
-        nota1 = findViewById(R.id.nota3_et);
-        nota1 = findViewById(R.id.nota4_et);
+        nota2 = findViewById(R.id.nota2_et);
+        nota3 = findViewById(R.id.nota3_et);
+        nota4 = findViewById(R.id.nota4_et);
         numeroFaltas = findViewById(R.id.faltas_et);
         btnCalcular = findViewById(R.id.calcular_btn);
         resultado = findViewById(R.id.resultado_tv);
