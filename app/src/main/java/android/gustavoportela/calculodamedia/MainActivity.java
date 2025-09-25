7e360ec49e7fda9e53c3ad09d96bfe32535765fa
GUSTAVO PORTELA DE LIMA <GUSTAVO_4657@uirapuru.br>
2025-09-25 15:15:56 -0300

    tela quase pronta

--- app/src/main/java/android/gustavoportela/calculodamedia/MainActivity.java
+++ app/src/main/java/android/gustavoportela/calculodamedia/MainActivity.java
@@ -17,7 +17,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
 
-    private EditText nota1, nota2, nota3, nota4, numeroFaltas;
+    private EditText nota1, nota2, nota3, nota4,nomeUsuario,numeroFaltas;
     private Button btnCalcular;
     private TextView resultado;
 
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         btnCalcular.setOnClickListener(view -> {
             validaCampos();
             calculaMedia();
+            receberNomeUser();
         });
 
 
@@ -49,27 +50,34 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+
+        private void receberNomeUser(){
+
+
+        }
+
     private void calculaMedia() {
+        String nomeUser = (nomeUsuario.getText().toString());
         double n1 = Double.parseDouble(nota1.getText().toString());
         double n2 = Double.parseDouble(nota2.getText().toString());
         double n3 = Double.parseDouble(nota3.getText().toString());
         double n4 = Double.parseDouble(nota4.getText().toString());
+
         double media = (n1 + n2 + n3 + n4) / 4;
         double faltas = Double.parseDouble(numeroFaltas.getText().toString());
-
-        if (media > 7) {
+        if (media >= 7) {
             if (faltas < 20) {
                 resultado.setTextColor(Color.parseColor("#437845"));
-                resultado.setText("aluno aprovado com média" + media);
+                resultado.setText("Olá " + nomeUser + " aprovado com média " + media);
             } else {
                 resultado.setTextColor(Color.parseColor("#F44336"));
-                resultado.setText("aluno com excesso de faltas" + faltas);
-
+                resultado.setText("Olá " + nomeUser + " com excesso de faltas: " + faltas);
             }
         } else {
             resultado.setTextColor(Color.parseColor("#F44336"));
-            resultado.setText("Aluno retido com média " + media);
+            resultado.setText("Olá " + nomeUser + " retido com média " + media);
         }
+
 
 
     }
@@ -83,13 +91,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initComponentes() {
-        nota1 = findViewById(R.id.nota1_et);
-        nota2 = findViewById(R.id.nota2_et);
-        nota3 = findViewById(R.id.nota3_et);
-        nota4 = findViewById(R.id.nota4_et);
-        numeroFaltas = findViewById(R.id.faltas_et);
+        nota1 = findViewById(R.id.nota1);
+        nota2 = findViewById(R.id.nota2);
+        nota3 = findViewById(R.id.nota3);
+        nota4 = findViewById(R.id.nota4);
+        nomeUsuario = findViewById(R.id.nomeUser);
+        numeroFaltas = findViewById(R.id.faltas);
         btnCalcular = findViewById(R.id.calcular_btn);
-        resultado = findViewById(R.id.resultado_tv);
+        resultado = findViewById(R.id.resultado);
 
 
     }
